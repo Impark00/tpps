52a884cf69d481cc4a6275f10155a8463f113fea
Impark00 <psj1197@naver.com>
2024-09-26 16:58:05 +0900

    saishokara

--- src/main/java/com/ps/tp/test/ttest.java
+++ src/main/java/com/ps/tp/test/ttest.java
@@ -1,5 +1,13 @@
 package com.ps.tp.test;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class ttest {
 
+	@GetMapping(value="/")
+	public String home () {
+		return "/home";
+	}
 }
